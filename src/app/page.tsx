5f28b8a17ae5d45f920ca22c7fe6b9fb30493393
Paulo Moutinho <paulo_vicali@icloud.com>
2023-05-23 13:00:38 -0300

    feat(develop-03): sessão da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
+import data from "../../data.json";
+
 export default function Home() {
   return (
     <div className="h-full bg-[url('back.jpeg')] bg-cover">
+      <div className="flex items-center justify-center flex-col mx-auto w-full">
+        <Image className="rounded-md mt-16" src="logo2.svg" alt={data.name} title={data.name} width="100" height="100"/>
+        <h1 className="font-bold text-xl mt-4">{data.name}</h1>
+        <p className="text-sm">{data.jobTilte}</p>
+      </div>
     </div>
   )
 }
