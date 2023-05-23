6c5b8eb470cf2d44a55b24ea73d6c63f7efa2031
Paulo Moutinho <paulo_vicali@icloud.com>
2023-05-23 19:37:30 -0300

    feat(develop-03): cria compoente de links

---
+++ src/components/LinkCard.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { LinkCardProps } from "@/types/type";
+
+export function LinkCard({ url, title }: LinkCardProps) {
+  return (
+    <Link
+      href={url}
+      passHref
+      target="_blank"
+      className="flex 
+        w-80 
+        max-h-16
+        md:w-[480px]
+        lg:w-[520px] 
+        p-4 
+        rounded-md 
+        hover:scale-105 
+        transition-all 
+        border-none 
+        mb-3 
+        bg-colorPalette"
+        rel="noreferrer"
+    >
+        <div className="flex justify-center items-center w-full">
+            <h2 className="font-semibold text-lg text-center sm:text-left leading-6">{title}</h2>
+        </div>
+    </Link>
+  );
+}
