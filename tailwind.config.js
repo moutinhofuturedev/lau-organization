28e9c5df7e8f1f1f03a4f242f168022edc668735
Paulo Moutinho <paulo_vicali@icloud.com>
2023-05-26 16:36:44 -0300

    feat(develop-06): insere footer e logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,6 @@ module.exports = {
       colorPalette: "#BFA598",
       white: "#FFFFFF",
     },
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
   },
   plugins: [],
 }
