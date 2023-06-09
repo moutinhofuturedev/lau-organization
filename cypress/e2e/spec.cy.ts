389441b29395804e105367ad461cdf03b9374713
Paulo Moutinho <paulo_vicali@icloud.com>
2023-06-09 01:36:59 -0300

    test(cypress-2): cria testes de página

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,13 @@
 describe('Home', () => {
-  it('visit the page', () => {
+  beforeEach('visit the page', () => {
     cy.visit('/');
 
     cy.title().should('contain', 'Lau Organização | Assessoria e Cerimonial');
   });
+
+  it("deve renderizar padrões de descrições da página", () => {
+    cy.get("h1").contains("LAU ORGANIZAÇÃO").should("be.visible");
+    cy.contains("Assessoria e Cerimonial").should("be.visible");
+    cy.contains("Copyright © Lau Organização 2023.Suzano, SP.").should("be.visible");
+  })
 });
