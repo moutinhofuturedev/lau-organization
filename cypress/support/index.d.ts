ac7c730f54bde380e699a33c580e4b7cbc6c3d92
Paulo Moutinho <paulo_vicali@icloud.com>
2023-06-10 12:33:48 -0300

    test(cypress-3): add namespace de commands

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,21 @@
+declare namespace Cypress {
+    interface Chainable {
+      /**
+       * Comando customizado para checar link de Assessoria Vip
+       * @example cy.checkLinkVip('link')
+       */
+      checkLinkVip(): Chainable<Element>;
+
+      /**
+       * Comando customizado para checar link do Pinterest
+       * @example cy.checkLinkPinteresr('link')
+       */
+      checkLinkPinterest(): Chainable<Element>;
+
+      /**
+       * Comando customizado para checar link de post do Instagram
+       * @example cy.checkLinkPost('link')
+       */
+      checkLinkPost(): Chainable<Element>;
+    }
+  }
