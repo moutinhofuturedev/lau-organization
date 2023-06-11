bc7c76de9c6ed83a9b03eaacfb9fa3a4c418f26d
Paulo Moutinho <paulo_vicali@icloud.com>
2023-06-11 01:33:22 -0300

    test(main): cria testes de componente whatsapp

---
+++ src/components/SocialIcons/__tests__/SocialIcons.spec.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { WhatsAppIcon } from '..';
+
+describe('WhatsAppIcon', () => {
+    it('should render the WhatsAppIcon component', () => {
+        // Arrange
+        render(<WhatsAppIcon />);
+        const svgElement = screen.getByTestId('svg-whatsapp');
+
+        // Assert
+        expect(svgElement).toBeInTheDocument();
+        expect(svgElement.tagName).toBe('svg');
+    });
+
+    it('should have the correct attributes and styles', () => {
+        // Arrange
+        render(<WhatsAppIcon />);
+        const svgElement = screen.getByTestId('svg-whatsapp');
+
+        // Assert
+        expect(svgElement).toHaveAttribute('xmlns', 'http://www.w3.org/2000/svg');
+        expect(svgElement).toHaveAttribute('width', '48');
+        expect(svgElement).toHaveAttribute('height', '49');
+        expect(svgElement).toHaveAttribute('fill', 'none');
+        expect(svgElement).toHaveAttribute('viewBox', '0 0 48 49');
+    });
+})      
