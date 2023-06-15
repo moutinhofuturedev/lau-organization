bf60c8c3ee1a206cb15293ab16d8a681cbf574ca
Paulo Moutinho <paulo_vicali@icloud.com>
2023-06-15 18:22:24 -0300

    ci(coverage): reportPaths

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,7 +1,6 @@
 import nextJest from 'next/jest.js';
  
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 });
  
