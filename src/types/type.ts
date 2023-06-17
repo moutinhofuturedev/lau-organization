c280e5e2d786fd43338ebbb7dc07f1d021b19a24
Paulo Moutinho <paulo_vicali@icloud.com>
2023-06-17 16:33:51 -0300

    feat(edge-config): tipagem para server component

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,4 +1,16 @@
-export interface LinkCardProps {
+export type LinkCardProps = {
   url: string
   title: string
+}
+
+type UnionProps = {
+  title: string
+  url: string
+}
+
+export type DataProps = {
+  name: string
+  jobTitle: string
+  links: UnionProps[]
+  social: UnionProps[]
 }
